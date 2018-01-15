496e8a8a264dc7ff35b87b267e4f86196d8a4ca5
Karen O <k.ortiz026@gmail.com>
2018-01-15 01:31:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function theBeatlesPlay(arrayM,arrayI){
+  //empty array instM
   var instM = [];
   for(let i=0;i<arrayM.length;i++){
+    //put string in the array
     instM.push(arrayM[i] +' plays '+ arrayI[i]);
   }
   return instM;
+}
+
+function johnLennonFacts(arrayF){
+  
 }
